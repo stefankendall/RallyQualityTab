eac39106feffab4722de0687158b8bc41e44d41a
eric.lifka <eric.lifka@gmail.com>
2012-12-13 15:14:50 -0500

    Remove toggle functionality

--- js/better_rally.js
+++ js/better_rally.js
@@ -1,34 +1,20 @@
 (function () {
-
-    chrome.extension.onMessage.addListener(
-        function (request, sender, sendResponse) {
-            if (request && request.refresh) {
-                location.reload();
+    function injectJs(links) {
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = links[0];
+        var callback = function () {
+            if (links.length > 1) {
+                injectJs(links.slice(1));
             }
-        }
-    );
+        };
+        script.addEventListener('load', callback, false);
 
-    chrome.extension.sendMessage({is_toggled_on:true}, function (response) {
-        if (response && response.is_toggled_on) {
-            function injectJs(links) {
-                var script = document.createElement("script");
-                script.type = "text/javascript";
-                script.src = links[0];
-                var callback = function () {
-                    if (links.length > 1) {
-                        injectJs(links.slice(1));
-                    }
-                };
-                script.addEventListener('load', callback, false);
+        (document.head || document.body || document.documentElement).appendChild(script);
+    }
 
-                (document.head || document.body || document.documentElement).appendChild(script);
-            }
-
-            injectJs([
-                chrome.extension.getURL("js/lib/jquery-1.8.2.min.js"),
-                chrome.extension.getURL("js/navigation/remove_quality_tab.js")
-            ]);
-        }
-    });
-
+    injectJs([
+        chrome.extension.getURL("js/lib/jquery-1.8.2.min.js"),
+        chrome.extension.getURL("js/navigation/remove_quality_tab.js")
+    ]);
 })();
