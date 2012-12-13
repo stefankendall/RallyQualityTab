2a7bbdd6d2ed39d918454306593a9bfa377417de
eric.lifka <eric.lifka@gmail.com>
2012-12-13 11:18:42 -0500

    Removing cruft

--- js/better_rally.js
+++ js/better_rally.js
@@ -13,12 +13,14 @@
     for (_optionIndex = 0; _optionIndex < RALLY_UTIL_OPTIONS.length; ++_optionIndex) {
         var optionKey = RALLY_UTIL_OPTIONS[_optionIndex];
         chrome.extension.sendMessage({method:"getLocalStorage", key:optionKey}, function (response) {
-            localStorage[optionKey] = JSON.stringify(response.data);
+            if (response) {
+                localStorage[optionKey] = JSON.stringify(response.data);
+            }
         });
     }
 
     chrome.extension.sendMessage({is_toggled_on:true}, function (response) {
-        if (response.is_toggled_on) {
+        if (response && response.is_toggled_on) {
             function injectJs(links) {
                 var script = document.createElement("script");
                 script.type = "text/javascript";
@@ -37,23 +39,7 @@
                 chrome.extension.getURL("js/lib/jquery-1.8.2.min.js"),
                 chrome.extension.getURL("js/lib/underscore-min.js"),
                 chrome.extension.getURL("js/lib/date.js"),
-                chrome.extension.getURL("js/util/query.js"),
-                chrome.extension.getURL("js/util/releases.js"),
                 chrome.extension.getURL("js/util/polling.js"),
-                chrome.extension.getURL("js/util/build_data.js"),
-                chrome.extension.getURL("js/editors/remove_bad_fields.js"),
-                chrome.extension.getURL("js/editors/filter_owners.js"),
-                chrome.extension.getURL("js/editors/remove_unused_story_menu_items.js"),
-                chrome.extension.getURL("js/editors/filter_kanban_states.js"),
-                chrome.extension.getURL("js/kanban/expand_all_kanban_cards.js"),
-                chrome.extension.getURL("js/kanban/add_claim_button_to_kanban_cards.js"),
-                chrome.extension.getURL("js/kanban/auto_release_selection.js"),
-                chrome.extension.getURL("js/kanban/add_inline_edit_fields_to_card.js"),
-                chrome.extension.getURL("js/kanban/setup_kanban_rally_link.js"),
-                chrome.extension.getURL("js/kanban/remove_unnecessary_filters.js"),
-                chrome.extension.getURL("js/kanban/hide_titlebar.js"),
-                chrome.extension.getURL("js/kanban/add_policy_fields_to_cards.js"),
-                chrome.extension.getURL("js/kanban/add_overclaimed_indicator.js"),
                 chrome.extension.getURL("js/navigation/trim_navigation_menu.js")
             ]);
         }
--- js/kanban/setup_kanban_rally_link.js
+++
@@ -1,7 +0,0 @@
-RallyUtil.waitForElementsAndExecute(['.sp-label'], function () {
-    $('.sp-label').css('font-size', '1.5em');
-    $('.sp-label').click(function () {
-        location.href = "https://rally1.rallydev.com/#/6895507658d/custom/3173483378";
-    });
-});
-
